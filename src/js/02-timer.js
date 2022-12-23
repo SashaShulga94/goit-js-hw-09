8e36471ae6b541b28fb1f05b8851fe21ec7ac389
SashaShulga94 <alexshulga.ss@gmail.com>
2022-12-23 22:02:10 +0200

    start 2task

---
+++ src/js/02-timer.js
@@ -0,0 +1,6 @@
+import flatpickr from 'flatpickr';
+
+import 'flatpickr/dist/flatpickr.min.css';
+
+const inputRef = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
