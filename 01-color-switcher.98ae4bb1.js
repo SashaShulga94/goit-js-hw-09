144d486829efb13aa0f9172d6fefa0f8d86da1e9
SashaShulga94 <109679965+SashaShulga94@users.noreply.github.com>
2022-12-23 16:01:49 +0000

    Deploying to gh-pages from @ SashaShulga94/goit-js-hw-09@3bec36b4c21990e7f1132dc6e5fdfc2870bda670 🚀

---
+++ 01-color-switcher.98ae4bb1.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),o=(document.querySelector("[data-stop]"),document.body);t.addEventListener("click",(function(){setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));o.style.backgroundColor=t}),1e3)}))}();
+//# sourceMappingURL=01-color-switcher.98ae4bb1.js.map
