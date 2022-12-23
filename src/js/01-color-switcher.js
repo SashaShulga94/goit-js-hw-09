3bec36b4c21990e7f1132dc6e5fdfc2870bda670
SashaShulga94 <alexshulga.ss@gmail.com>
2022-12-23 18:01:10 +0200

    1task in process

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
-console.log('hello');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyColor = document.body;
+
+let currentClr = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+startBtn.addEventListener('click', () => {
+  currentClr = setInterval(() => {
+    const color = getRandomHexColor();
+    bodyColor.style.backgroundColor = color;
+  }, 1000);
+});
+
+// startBtn.addEventListener('click', startChangeClr);
+// stopBtn.addEventListener('click', stopChangeClr);
+
+// const colorField = function startChangeClr(evt) {
+//   const color = getRandomHexColor();
+//   bodyColor.style.backgroundColor = color;
+//   console.dir(evt.currentTarget);
+// };
+
+// stopBtn.addEventListener('click', () => {
+//   clearInterval(timerId);
+//   console.log(`Interval with id ${timerId} has stopped!`);
+// });
