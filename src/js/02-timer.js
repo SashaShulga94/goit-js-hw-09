ca6fd48c8457d45ba0c21526b877213f2c103d11
SashaShulga94 <alexshulga.ss@gmail.com>
2022-12-24 18:02:35 +0200

    start 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,3 +4,17 @@ import 'flatpickr/dist/flatpickr.min.css';
 
 const inputRef = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('[data-start]');
+
+flatpickr(selector, options);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+// if (selectedDates !== ) {
+//   window.alert('Please choose a date in the future');
+// }
